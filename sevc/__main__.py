bd5196df410a8497abd06c3df9d5728587cfd822
Andrew Martin <andrewgmartin86@outlook.com>
2021-05-22 00:37:51 +0100

    Lots of options

--- sevc/__main__.py
+++ sevc/__main__.py
@@ -1,7 +1,55 @@
+import argparse
 import os
+import sevc
+import sys
 
+from sevc.locations import Location
 from sevc.settings import Settings
+from sevc.tariffs import Tariff
+from sevc.vehicles import Vehicle
 
+
+arg_parser = argparse.ArgumentParser(description='Optimise electric vehicle charging to cheapest rates')
+
+action_group = arg_parser.add_mutually_exclusive_group()
+
+action_group.add_argument(
+    '-s',
+    '--list',
+    action='store_true',
+    help='Combine with -l, -t and/or -v (with optional ID) to list those assets. Omit -l/-t/-v to list everything.'
+)
+
+action_group.add_argument(
+    '-n',
+    '--new',
+    action='store_true',
+    help='Must be combined with -l, -t and/or -v (without an ID) to create a new asset.'
+)
+
+action_group.add_argument(
+    '-d',
+    '--delete',
+    action='store_true',
+    help='Must be combined with -l, -t and/or -v (with an ID) to delete that asset.'
+)
+
+arg_parser.add_argument('-l', '--location', type=int, nargs='*', default=None)
+arg_parser.add_argument('-t', '--tariff', type=int, nargs='*', default=None)
+arg_parser.add_argument('-v', '--vehicle', type=int, nargs='*', default=None)
+
+try:
+    args = arg_parser.parse_args()
+except argparse.ArgumentError:
+    print('Invalid arguments')
+    sys.exit(2)
+
+action_defined = args.list or args.new or args.delete
+asset_defined = args.location is not None or args.tariff is not None or args.vehicle is not None
+
+if action_defined != asset_defined and not args.list:
+    print('Invalid arguments')
+    sys.exit(2)
 
 base_dir = os.path.realpath(os.path.dirname(__file__) + '/..')
 
@@ -10,8 +58,111 @@ if not os.path.isdir(base_dir + '/var'):
 
 settings = Settings(base_dir + '/var/sevc.json')
 
-for uuid in settings.tariffs:
-    settings.tariffs[uuid]()
+if not action_defined:
+    for uuid in settings.tariffs:
+        settings.tariffs[uuid]()
 
-for uuid in settings.vehicles:
-    settings.vehicles[uuid](settings.locations, settings.tariffs)
+    for uuid in settings.vehicles:
+        settings.vehicles[uuid](settings.locations, settings.tariffs)
+
+    sys.exit(0)
+
+if args.list:
+    if args.location is not None or not asset_defined:
+        print('LOCATIONS')
+        i = 0
+        for uuid in settings.locations:
+            i += 1
+            if args.location is not None and len(args.location) > 0 and i not in args.location:
+                continue
+            print(str(i) + ': ' + settings.locations[uuid].name)
+        print()
+
+    if args.tariff is not None or not asset_defined:
+        print('TARIFFS')
+        i = 0
+        for uuid in settings.tariffs:
+            i += 1
+            if args.tariff is not None and len(args.tariff) > 0 and i not in args.tariff:
+                continue
+            print(str(i) + ': ' + settings.tariffs[uuid].name)
+        print()
+
+    if args.vehicle is not None or not asset_defined:
+        print('VEHICLES')
+        i = 0
+        for uuid in settings.vehicles:
+            i += 1
+            if args.vehicle is not None and len(args.vehicle) > 0 and i not in args.vehicle:
+                continue
+            print(str(i) + ': ' + settings.vehicles[uuid].name)
+        print()
+
+    sys.exit(0)
+
+if args.delete:
+    if args.location is not None:
+        if len(args.location) == 0:
+            print('Missing locations')
+            sys.exit(2)
+        i = 0
+        print('Deleting locations...')
+        for uuid in settings.locations:
+            i += 1
+            if i in args.location:
+                print(settings.locations[uuid].name)
+                settings.locations.pop(uuid)
+        print()
+
+    if args.tariff is not None:
+        if len(args.tariff) == 0:
+            print('Missing tariffs')
+            sys.exit(2)
+        i = 0
+        print('Deleting tariffs...')
+        for uuid in settings.tariffs:
+            i += 1
+            if i in args.tariff:
+                deleting = True
+                for loc in settings.locations:
+                    if settings.locations[loc].tariff == uuid:
+                        print('Cannot delete ' + settings.tariffs[uuid].name)
+                        deleting = False
+                        break
+                if deleting:
+                    print(settings.tariffs[uuid].name)
+                    settings.tariffs.pop(uuid)
+        print()
+
+    if args.vehicle is not None:
+        if len(args.vehicle) == 0:
+            print('Missing vehicles')
+            sys.exit(2)
+        i = 0
+        print('Deleting vehicles...')
+        for uuid in settings.vehicles:
+            i += 1
+            if i in args.vehicle:
+                print(settings.vehicles[uuid].name)
+                settings.vehicles.pop(uuid)
+        print()
+
+    settings.save()
+    sys.exit(0)
+
+if args.new:
+    if args.tariff is not None:
+        tariff = sevc.instantiate_subclass(Tariff)
+        settings.tariffs[tariff.uuid] = tariff
+
+    if args.location is not None:
+        location = sevc.instantiate_subclass(Location)
+        settings.locations[location.uuid] = location
+
+    if args.vehicle is not None:
+        vehicle = sevc.instantiate_subclass(Vehicle)
+        settings.vehicles[vehicle.uuid] = vehicle
+
+    settings.save()
+    sys.exit(0)
+
